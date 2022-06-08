ac6a5a6da43b984cf5f99ee9f5618a757a6ef44d
Khalid Warsame <khalid_warsame1@hotmail.com>
2022-06-08 01:34:24 +0100

    init

---
+++ Temperature.js
@@ -0,0 +1,9 @@
+function multiplyByNineFifths(number) {
+  return number * (9/5);
+};
+ 
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+};
+ 
+getFahrenheit(15); // Returns 59
